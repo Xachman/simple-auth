c516475dc033863a2ebc1923a5f0cfbf717f25de
Xach <zironside@hotmail.com>
2018-04-15 16:29:07 -0400

    user host and port environemnt vars for proxy_pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 
 echo -n $USER: >> /etc/nginx/.htpasswd
 openssl passwd -apr1 $PASSWORD >> /etc/nginx/.htpasswd
-
-
-echo "$@"
+sed -i "s/proxy_pass http:\/\/.*/proxy_pass http:\/\/$HOST:$PORT\/;/" /etc/nginx/conf.d/default.conf 
 
 exec "$@"
